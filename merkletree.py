d30e7791d060e1d1ca8736582cafdd0a31c699cc
Zebartin <1500012724@pku.edu.cn>
2021-10-22 22:28:09 +0800

    style: comments for MerkleTree

--- merkletree.py
+++ merkletree.py
@@ -1,3 +1,9 @@
+"""Merkle Tree在比特币系统中的简单实现
+
+使用样例：
+
+merkle_tree = MerkleTree.make_merkle_tree(['big', 'brother', 'is', 'watching', 'you'])
+"""
 from __future__ import annotations
 from collections.abc import Iterable
 import logging
@@ -6,6 +12,14 @@ from utils import double_hash
 
 
 class MerkleNode:
+    """Merkle树的节点
+
+    属性：
+        hash: 节点的hash值
+        left: 左子节点
+        right: 右子节点
+    """
+
     def __init__(self, data: str) -> None:
         if not isinstance(data, str):
             raise TypeError('Merkle树节点数据应为str类型')
@@ -15,6 +29,7 @@ class MerkleNode:
 
     @staticmethod
     def parent(left: MerkleNode, right: MerkleNode) -> MerkleNode:
+        """由两个节点计算得到它们的父节点"""
         parent_node = MerkleNode(left.hash + right.hash)
         parent_node.left = left
         parent_node.right = right
@@ -25,15 +40,26 @@ class MerkleNode:
 
 
 class MerkleTree:
+    """Merkle树
+
+    属性：
+        root: 根节点
+    """
+
     def __init__(self, data) -> None:
         if not isinstance(data, Iterable):
             raise TypeError('Merkle树的data应为可迭代对象')
         layer_nodes = [MerkleNode(x) for x in data]
+        if len(layer_nodes) == 0:
+            return None
+        # 自底向上构建merkle树
         while len(layer_nodes) > 1:
+            # 如果是奇数个节点，重复最后一个节点
             if len(layer_nodes) % 2:
                 layer_nodes.append(layer_nodes[-1])
-            assert(len(layer_nodes) % 2 == 0)
+            assert len(layer_nodes) % 2 == 0
 
+            # 在layer_nodes中原地生成每层的节点
             for i in range(len(layer_nodes) // 2):
                 layer_nodes[i] = MerkleNode.parent(
                     layer_nodes[i * 2], layer_nodes[i * 2 + 1])
@@ -45,4 +71,5 @@ class MerkleTree:
 
     @staticmethod
     def make_merkle_tree(data) -> MerkleTree:
+        """生成merkle树"""
         return MerkleTree(data)
