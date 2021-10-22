ec736064d59c3273d2b94aa65019f0da6da8b1f9
Zebartin <1500012724@pku.edu.cn>
2021-10-22 22:28:25 +0800

    style: comments for utils

--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ import random
 import string
 
 def double_hash(x: str) -> str:
+    """两次SHA256哈希"""
     return hashlib.sha256(hashlib.sha256(x.encode()).hexdigest().encode()).hexdigest()
 
 def random_str(num: int = 16):
+    """随机生成num长度的字符串"""
     while True:
         yield ''.join(random.sample(string.ascii_letters+string.digits+string.punctuation, num))
